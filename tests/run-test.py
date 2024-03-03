261cd792ec0d49ee9fc1439a846e22aa789a03ed
indamutsa <arsichizy@gmail.com>
2024-03-03 14:40:38 +0100

    working rate printout...

--- tests/run-test.py
+++ tests/run-test.py
@@ -11,19 +11,23 @@ start_time = 0
 
 def calculate_current_rate(start_rate, target_rate, transition_duration, elapsed_time):
     if transition_duration <= 0:
-        # print("Transition duration must be greater than 0 ==> Current: ",current_rate, "Target: ",target_rate,"Start: ", start_rate, "Elapsed: ", elapsed_time, "Transition: ", transition_duration)
         return target_rate
-    b = math.log(start_rate / target_rate) / transition_duration
-    current_rate = (start_rate - target_rate) * math.exp(-b * elapsed_time) + target_rate
-    # print("|| ==> Current: ",current_rate, "Target: ",target_rate,"Start: ", start_rate, "Elapsed: ", elapsed_time, "Transition: ", transition_duration)
+    # Calculate the rate of change
+    rate_of_change = math.log(target_rate / start_rate) / transition_duration
+    # Calculate the current rate based on elapsed time
+    current_rate = start_rate * math.exp(rate_of_change * elapsed_time)
 
+    # Ensure the current rate does not overshoot in either direction
     if start_rate < target_rate:
-        current_rate = int(min(current_rate, target_rate))
-        # print("Start is less than target ==> Current: ",current_rate, "Target: ",target_rate,"Start: ", start_rate, "Elapsed: ", elapsed_time, "Transition: ", transition_duration)
+        current_rate = min(current_rate, target_rate)
+        # print("Current Rate>>>: ", current_rate)
     else:
-        current_rate = int(max(current_rate, target_rate))
-        # print("Target is greater than start ==> Current: ",current_rate, "Target: ",target_rate,"Start: ", start_rate, "Elapsed: ", elapsed_time, "Transition: ", transition_duration)
+        current_rate = max(current_rate, target_rate)
+        # print("Current Rate: ", current_rate, target_rate)
 
+    # Optional: Adjust to integer if necessary
+    # current_rate = int(current_rate)
+    
     return current_rate
 
 def generate_change_intervals(total_duration_sec):
@@ -34,9 +38,9 @@ def generate_change_intervals(total_duration_sec):
     return sorted(randomized_sample)
 
 
-def print_at_rate(message="Printing at the machine's rate..."):
+def print_at_rate( transition_duration, change_type,message="Printing at the machine's rate..."):
     elapsed_time = time.time() - start_time  # Calculate elapsed time since the start
-    print(f"{message} | Current Rate: {current_rate_ms:.2f}ms | Elapsed Time: {elapsed_time:.2f}s | Current Window: {current_window}")
+    print(f"{message} | Current Rate: {current_rate_ms:.2f}ms | Elapsed Time: {elapsed_time:.2f}s | Change type: {change_type} |Transition: {transition_duration}| Current Window: {current_window}")
     time.sleep(current_rate_ms / 1000)
 
 
@@ -61,41 +65,84 @@ def simulate_window_acceleration( start_time,change_windows, transition_duration
                 current_rate_ms = calculate_current_rate(initial_rate_ms, stable_rate_ms, transition_duration, elapsed_time)
             else:
                 current_rate_ms = stable_rate_ms
-            print_at_rate()
+            print_at_rate(transition_duration, "accelerate")
         # return current_rate_ms, elapsed_time
     else:
         # Change type: accelerate or decelerate
-        change_type_ = random.choice(['accelerate', 'decelerate']) if current_window + 1 in change_windows else None
+        updated_current_window = current_window + 1
+        change_type = random.choice(['accelerate', 'decelerate']) if updated_current_window in change_windows else None
 
         rand_start = (current_window * change_window_size) + random.randint(1,8)
-        transition_duration =  min(((current_window + 1) * change_window_size) - rand_start, transition_duration)
-        window_limit = (current_window + 1) * change_window_size
-        change_type = 'accelerate'
+        transition_duration =  min(((updated_current_window) * change_window_size) - rand_start, transition_duration)
+        iteration_stop = rand_start + transition_duration
+        window_limit = (updated_current_window) * change_window_size
+        # change_type = random.choice(['accelerate', 'decelerate'])
+        # change_windows = [2, 3, 4, 5, 6]
+        # rand_start = 12
+        print("Change Type: ", change_type, " Rand Start, Stop: ", rand_start, iteration_stop)
 
-        print(f"Change Type: {change_type_} | Rand_start: {rand_start} | Transition Duration: {transition_duration} | Elapsed Time: {elapsed_time} | Window Limit: {window_limit}")
-        
+        # print(f"Change Type: {change_type} | Rand_start: {rand_start} | Stopping at {iteration_stop} | Transition Duration: {transition_duration} | Elapsed Time: {elapsed_time} | Window Limit: {window_limit}")
+        a = False
 
         while elapsed_time < window_limit:
+
             elapsed_time = time.time() - start_time
-            inner_lapsed_time = elapsed_time % 10
+            # inner_lapsed_time = elapsed_time % 10 
 
-            if change_type == 'accelerate':
-                # print("We are accelerating... from 400ms to 100ms, Elaspesd Time:", inner_lapsed_time)
-    
-                if elapsed_time <= 16:
-                #    print("000000===  Elapsed_time:", elapsed_time, "Rand Start:", rand_start-10, "Transition Duration:", transition_duration, "Inner Lapsed Time:", inner_lapsed_time)
-                   current_rate_ms = calculate_current_rate(stable_rate_ms, max_acceleration_rate_ms, transition_duration, inner_lapsed_time)
-                else:
-                    current_rate_ms = max_acceleration_rate_ms
-                    # print("we are out of the transition period")
+            if change_type == 'accelerate' and updated_current_window in change_windows:               
+                if elapsed_time >= rand_start and elapsed_time <= iteration_stop:
                     
-            elif change_type == 'decelerate':
-                # print("We are decelerating... from 100ms to 400ms, Elaspesd Time:", elapsed_time)
+                    if  current_rate_ms <= stable_rate_ms:
+                        if a == False:
+                            inner_start_time = time.time()
+                            a = True
+                            
+                        inner_lapsed_time = time.time() - inner_start_time 
+                        # print("Inner Lapsed Time: ", inner_lapsed_time)
 
-                current_rate_ms = calculate_current_rate(max_deceleration_rate_ms, stable_rate_ms, transition_duration, elapsed_time)
-            print(f"Change Type: {change_type_} | Rand_start: {rand_start} | Transition Duration: {transition_duration} | Elapsed Time: {elapsed_time} | Window Limit: {window_limit}")
-            print_at_rate()
-        sys.exit(0)    
+                        current_rate_ms = calculate_current_rate(current_rate_ms, max_acceleration_rate_ms, transition_duration, inner_lapsed_time) 
+                        # print("***1", current_rate_ms)
+
+                       
+                    else:
+                        if a == False:
+                            inner_start_time = time.time()
+                            a = True
+                            
+                        inner_lapsed_time = time.time() - inner_start_time
+                        # print("Inner Lapsed Time: ", inner_lapsed_time) 
+                        current_rate_ms = calculate_current_rate( current_rate_ms, stable_rate_ms, transition_duration, inner_lapsed_time)
+                        # print("***2", current_rate_ms)
+
+                else:
+                    current_rate_ms = current_rate_ms
+            elif change_type == 'decelerate' and updated_current_window in change_windows:
+                if elapsed_time >= rand_start and elapsed_time <= iteration_stop:
+                    
+                    if current_rate_ms >= stable_rate_ms:
+                        if a == False:
+                            inner_start_time = time.time()
+                            a = True
+                            
+                        inner_lapsed_time = time.time() - inner_start_time
+                        # print("Inner Lapsed Time: ", inner_lapsed_time)                        
+                        current_rate_ms = calculate_current_rate(current_rate_ms, max_deceleration_rate_ms, transition_duration, inner_lapsed_time) 
+                        # print("***3", current_rate_ms)
+                    
+                    else:
+
+                        if a == False:
+                            inner_start_time = time.time()
+                            a = True
+                            
+                        inner_lapsed_time = time.time() - inner_start_time
+                        # print("Inner Lapsed Time: ", inner_lapsed_time) 
+                        current_rate_ms = calculate_current_rate(current_rate_ms, stable_rate_ms, transition_duration, inner_lapsed_time)
+                        # print("***4", current_rate_ms)
+
+            # print(f"Change Type: {change_type} | Rand_start: {rand_start} | Transition Duration: {transition_duration} | Elapsed Time: {elapsed_time} | Window Limit: {window_limit}")
+            print_at_rate(transition_duration, change_type)
+        # sys.exit(0)    
         # return current_rate_ms, elapsed_time
 
 
